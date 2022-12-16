fd17afe50747af8c7b319737f976a322bf7596dd
Sherif <sherifmagdy@gmail.com>
2022-12-16 10:56:10 +0100

    Part 3

--- nobel_prize.py
+++ nobel_prize.py
@@ -38,11 +38,96 @@ cat = {"fysik": "phy",
 # TODO 15p if the user does not specify a field such as physics or chemistry, then that parameter should not be sent to the api and we will then receive all the prizes for that year
 
 
+# calculation money for each prize
+#  variables: money:total money
+#             prize_cnt : awarded person's count
+def calcMoneyForEachPrize(money: float, prize_cnt: float):
+    average_money = money / prize_cnt
+    res = round(average_money, 3)
+    return res
+
+
+# check if the field is correct
+def checkfield(field: str):
+    if field not in cat:
+        return False
+    else:
+        return True
+
+
+# def get prizers from year and field
+def getInforamationFromServer(year: int, field: str):
+    params = {"nobelPrizeYear": year, "nobelPrizeCategory": field}
+    res = requests.get("http://api.nobelprize.org/2.1/nobelPrizes", params=params).json()
+    return res
+
+
+# print for year and field
+def printOneFieldForYear(year: int, field: str):
+    res = getInforamationFromServer(int(year), field)
+
+    for p in res["nobelPrizes"]:
+        print("----------------------------")
+        peng = p["prizeAmount"]
+        idagpeng = p["prizeAmountAdjusted"]
+        print(f"{p['categoryFullName']['se']} prissumma {peng} SEK")
+        prize_cnt = 0
+
+        for m in p["laureates"]:
+            print("----------------------------")
+            if "knownName" in m:
+                print(m['knownName']['en'])
+            print(m['motivation']['en'])
+            andel = m['portion']
+            prize_cnt += 1
+        print("*" * 30)
+        money_for_thattime = calcMoneyForEachPrize(peng, prize_cnt)
+        result1 = f'{money_for_thattime:.3f}'
+        print(f"The money of the time for each prizer is {result1}")
+
+        money_for_now = calcMoneyForEachPrize(idagpeng, prize_cnt)
+        result2 = f'{money_for_now:.3f}'
+        print(f"The Today's value for each prizer is {result2}")
+
+
+# print all informations for year
+def printAllInformationsForYear(year: int):
+    for item in cat:
+        print("*" * 30)
+        print(f"Field is {item}")
+        res = getInforamationFromServer(int(year), item)
+
+        for p in res["nobelPrizes"]:
+            print("----------------------------")
+            peng = p["prizeAmount"]
+            idagpeng = p["prizeAmountAdjusted"]
+            print(f"{p['categoryFullName']['se']} prissumma {peng} SEK")
+            print(f"{p['categoryFullName']['se']} prissumma {idagpeng} SEK")
+            prize_cnt = 0
+
+            for m in p["laureates"]:
+                print("----------------------------")
+                if "knownName" in m:
+                    print(m['knownName']['en'])
+                print(m['motivation']['en'])
+                andel = m['portion']
+                prize_cnt += 1
+
+            print("*" * 30)
+            money_for_thattime = calcMoneyForEachPrize(peng, prize_cnt)
+            result1 = f'{money_for_thattime:.3f}'
+            print(f"The money of the time for each prizer is {result1}")
+
+            money_for_now = calcMoneyForEachPrize(idagpeng, prize_cnt)
+            result2 = f'{money_for_now:.3f}'
+            print(f"The Today's value for each prizer is {result2}")
+
+
 def main():
     print(HELP_STRING)
 
     while True:
-        menu_choice = input("Make a choice? \n(You can enter 1,2,h(H) or q(Q)) ").upper().strip()
+        menu_choice = input("\nMake a choice? \n(You can enter 1,2,h(H) or q(Q)) \n").upper().strip()
         # TODO 5p Skriv bara ut hjälptexten en gång när programmet startar inte efter varje gång användaren matat in en fråga
         #      Förbättra hjälputskriften så att användaren vet vilka fält, exempelvis kemi som finns att välja på
 
@@ -59,33 +144,26 @@ def main():
         # TODO 5p Make the help text printed if the user types h or H
 
         if menu_choice == '1':
+            field = ""
+            year = ""
             aaa = input(">")
-            a, b = aaa.split()
+            str_list = aaa.split()
+            flag = "All"
+            if (len(str_list) == 1):
+                flag = "All"
+                year = str_list[0]
+            else:
+                flag = "OneField"
+                year, field = aaa.split()
 
-            if b not in cat:
+            if flag == "OneField" and not checkfield(field):
                 print("You should print correct field.\n You can find the fields select 2")
             else:
-                c = cat[b]
-                c = {"nobelPrizeYear": int(a), "nobelPrizeCategory": c}
+                if (flag == "OneField"):
+                    printOneFieldForYear(int(year), field)
+                else:
+                    printAllInformationsForYear(int(year))
 
-                res = requests.get("http://api.nobelprize.org/2.1/nobelPrizes", params=c).json()
-                # TODO 5p  Lägg till någon typ av avskiljare mellan pristagare, exempelvis --------------------------
-
-                # TODO 20p Skriv ut hur mycket pengar varje pristagare fick, tänk på att en del priser delas mellan flera mottagare, skriv ut både i dåtidens pengar och dagens värde
-                #   Skriv ut med tre decimalers precision. exempel 534515.123
-                #   Skapa en funktion som hanterar uträkningen av prispengar och skapa minst ett enhetestest för den funktionen
-                #   Tips, titta på variabeln andel
-                # Feynman fick exempelvis 1/3 av priset i fysik 1965, vilket borde gett ungefär 282000/3 kronor i dåtidens penningvärde
-
-                for p in res["nobelPrizes"]:
-                    peng = p["prizeAmount"]
-                    idagpeng = p["prizeAmountAdjusted"]
-                    print(f"{p['categoryFullName']['se']} prissumma {peng} SEK")
-
-                    for m in p["laureates"]:
-                        print(m['knownName']['en'])
-                        print(m['motivation']['en'])
-                        andel = m['portion']
         if menu_choice == '2':
             print("Here, you can see the fields")
             for item in cat:
